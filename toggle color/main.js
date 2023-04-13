9b58ee5d50e7f95b692854c498eacd57c7796bdf
solimanghandour <[200~solimanghandour21@gmail.com>
2023-04-13 05:06:36 +0200

    upload_toggel_color

---
+++ toggle color/main.js
@@ -0,0 +1,15 @@
+var x=1
+function toggle(){
+    if(x==1){
+        document.getElementById('div').style.backgroundColor = "Magenta"
+                      x=2
+    }
+    
+ 
+    else{
+        document.getElementById('div').style.backgroundColor = "Red"
+        x=1
+
+    }
+ 
+}
